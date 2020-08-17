17ed38a081c8e1db45e49fe4d8c3466a34dcf452
teriberka <box4trash@inbox.ru>
2020-08-17 07:46:26 +0300

    commit 1 | hometask 02 | Второе задание полностью.

--- app/src/main/java/ru/geekbrains/weatherforecast/MainActivity.java
+++ app/src/main/java/ru/geekbrains/weatherforecast/MainActivity.java
@@ -1,14 +1,26 @@
 package ru.geekbrains.weatherforecast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView mAutoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_city);
+
+        final String[] countries = getResources().getStringArray(R.array.cities);
+
+        mAutoCompleteTextView = findViewById(R.id.autoCompleteTextViewCity);
+        mAutoCompleteTextView.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, countries));
+
     }
 }
